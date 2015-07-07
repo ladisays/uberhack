ceef195d8cb07715710053f0c962e1293dc26372
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-07 15:06:15 +0100

    change req.param to req.query

--- api/routes/calendar.routes.js
+++ api/routes/calendar.routes.js
@@ -7,20 +7,60 @@ _ = require('lodash');
 module.exports = function(app, config) {
   var root = new Firebase(config.firebase.rootRefUrl),
     auth = new googleAuth(),
-    oAuthClient = new auth.OAuth2(config.calendar.clientId, config.calendar.clientSecret, config.calendar.callBackURL);
+    oAuthClient = new auth.OAuth2(config.calendar.clientId, config.calendar.clientSecret, config.calendar.callBackURL),
+    authed = false;
 
   app.route('/calendar')
     .get(function(req, res) {
-      var url = oAuthClient.generateAuthUrl({
-        access_type: 'offline',
-        scope: 'https://www.googleapis.com/auth/calendar.readonly'
-      });
-      res.redirect(url);
+      if (!authed) {
+        var url = oAuthClient.generateAuthUrl({
+          access_type: 'offline',
+          scope: 'https://www.googleapis.com/auth/calendar.readonly'
+        });
+        res.redirect(url);
+      } else {
+        var calendar = google.calendar('v3');
+        calendar.events.list({
+          calendarId: 'primary',
+          maxResults: 10,
+          timeMin: (new Date()).toISOString(),
+          singleEvents: true,
+          orderBy: 'startTime',
+          auth: oAuthClient
+        }, function(err, response) {
+          if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+          }
+          var events = response.items;
+          if (events.length === 0) {
+            console.log('No upcoming events found.');
+            return res.json({
+              err: 'No upcoming events found.'
+            });
+          } else {
+            console.log('Upcoming 10 events: ', events);
+            var selectedEvents = [];
+            for (var i = 0; i < events.length; i++) {
+              var event = events[i],
+                eventDetails = {};
+              eventDetails.start = event.start.dateTime || event.start.date;
+              eventDetails.end = event.end.dateTime || event.end.date;
+              eventDetails.status = event.status;
+              eventDetails.location = event.location;
+              eventDetails.organiser = event.organiser;
+              eventDetails.summary = event.summary;
+              selectedEvents.push(eventDetails);
+            }
+            return res.json(selectedEvents);
+          }
+        });
+      }
     });
 
   app.route('/calendar/callback')
     .get(function(req, res) {
-      var code = req.param('code');
+      var code = req.query.code;
       oAuthClient.getToken(code, function(err, tokens) {
         if (err) {
           console.log('Error authenticating');
@@ -28,52 +68,10 @@ module.exports = function(app, config) {
         } else {
           console.log('Successfully authenticated');
           console.log(tokens);
-          // Store our credentials and redirect back to our main page
           oAuthClient.setCredentials(tokens);
-          res.redirect('/calendar/process');
+          authed = true;
+          res.redirect('/calendar');
         }
       });
-    });
-
-  app.route('/calendar/process')
-    .get(function(req, res) {
-      var calendar = google.calendar('v3');
-      calendar.events.list({
-        calendarId: 'primary',
-        maxResults: 10,
-        timeMin: (new Date()).toISOString(),
-        singleEvents: true,
-        orderBy: 'startTime',
-        auth: oAuthClient
-      }, function(err, response) {
-        if (err) {
-          console.log('The API returned an error: ' + err);
-          return;
-        }
-        // console.log(response);
-        var events = response.items;
-        if (events.length === 0) {
-          console.log('No upcoming events found.');
-          return res.json({
-            err: 'No upcoming events found.'
-          });
-        } else {
-          console.log('Upcoming 10 events: ', events);
-          var selectedEvents = [];
-          for (var i = 0; i < events.length; i++) {
-            var event = events[i],
-              eventDetails = {};
-            eventDetails.start = event.start.dateTime || event.start.date;
-            eventDetails.end = event.end.dateTime || event.end.date;
-            eventDetails.status = event.status;
-            eventDetails.location = event.location;
-            eventDetails.organiser = event.organiser;
-            eventDetails.summary = event.summary;
-            selectedEvents.push(eventDetails);
-          }
-          return res.json(selectedEvents);
-        }
-      });
-
     });
 };
