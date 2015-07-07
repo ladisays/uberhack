1c405ca46ecb4799764950b882a8bacff0884216
andela-koni <oni.omowunmi@andela.co>
2015-07-07 13:21:51 +0100

    fixed conflict

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,7 +1,30 @@
-var Firebase = require('firebase'),
-needle = require('needle');
-_ = require('lodash');
+var Firebase = require('firebase');
+var needle       = require('needle');
+var _            = require('lodash');
+var OAuth        = require('oauth');
+var requests     = require('request')
 
 module.exports = function(app, config) {
+
   var root = new Firebase(config.firebase.rootRefUrl);
+
+  var OAuth2 = OAuth.OAuth2;
+
+  var oauth2 = new oAuth2(
+    config.firebase.uber.clientId,
+    config.firebase.secretKey,
+    "uberhack",
+    config.firebase.uber.authorize_url,
+    config.firebase.uber.access_token_url,
+    config.firebase.uber.base_uber_url
+  )
+
+  parameters = {
+    'response_type': 'code',
+    'redirect_uri': config.firebase.uber.redirect_url,
+    'scope': 'profile'
+  }
+
+  login_url = oauth2.getAuthorizeUrl(parameters)
+
 };
--- config/config.js
+++ config/config.js
@@ -7,8 +7,23 @@ var development = {
     clientId: '453264479059-fc56k30fdhl07leahq5n489ct7ifk7md.apps.googleusercontent.com',
     clientSecret: 'XA-7mkghxVKC-iXkCR9VnAbe',
     callBackURL: 'http://localhost:5555/calendar/callback'
+  },
+  uber: {
+    clientId:            'rr2NzvHi69QJalUHz0ImU1KidoE1KGc5'
+    access_token_url:    "https://login.uber.com/oauth/token",
+    authorize_url:       "https://login.uber.com/oauth/authorize",
+    base_url:            "https://login.uber.com/",
+    scopes:              ["profile", "history_lite"],
+    base_uber_url:       "https://api.uber.com/v1/",
+    base_uber_url_v1_1 : "https://api.uber.com/v1.1/",
+    redirect_url:        "http://localhost/uber/callback"
+    // start_latitude:      "37.781955",
+    // start_longitude:     "-122.402367",
+    // end_latitude:        "37.744352",
+    // end_longitude:       "-122.416743"
   }
 };
+
 
 var test = {
   firebase: {
