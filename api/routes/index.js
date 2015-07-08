b7d1c253709ebff587fd6bec4bb23c4556011cd0
Ladi Adenusi andela-ladenusi <ladi.adenusi@andela.co>
2015-07-08 10:21:10 +0100

    added function to keep heroku app alive

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,13 @@
 var users 			= require('./users.routes'),
 		calendar 		= require('./calendar.routes'),
 		requests 		= require('./requests.routes'),
-		shared 			= require('./shared.routes');
+		shared 			= require('./shared.routes'),
+		keepalive		= require('./keep-alive.routes');
 
 module.exports = function(app, config) {
   users(app, config);
   calendar(app, config);
   requests(app, config);
   shared(app, config);
+  keepalive(app, config);
 };
