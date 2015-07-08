3235d1d1726fd97924aa08174aae0e7288de6528
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-08 13:16:54 +0100

    save calendatr to firebase

--- api/routes/calendar.routes.js
+++ api/routes/calendar.routes.js
@@ -1,8 +1,8 @@
-var Firebase      = require('firebase'),
-  needle          = require('needle'),
-  google          = require('googleapis'),
-  googleAuth      = require('google-auth-library'),
-  _               = require('lodash');
+var Firebase = require('firebase'),
+  needle = require('needle'),
+  google = require('googleapis'),
+  googleAuth = require('google-auth-library'),
+  _ = require('lodash');
 
 module.exports = function(app, config) {
   var root = new Firebase(config.firebase.rootRefUrl),
@@ -10,23 +10,25 @@ module.exports = function(app, config) {
     oAuthClient = new auth.OAuth2(config.calendar.clientId, config.calendar.clientSecret, config.calendar.callBackURL),
     authed = false;
 
-  app.route('/calendar')
+  app.route('/:uid/calendar')
     .get(function(req, res) {
+      uid = req.params.uid;
+      console.log(uid);
       if (!authed) {
         var url = oAuthClient.generateAuthUrl({
-          access_type   : 'offline',
-          scope         : 'https://www.googleapis.com/auth/calendar.readonly'
+          access_type: 'offline',
+          scope: 'https://www.googleapis.com/auth/calendar.readonly'
         });
         res.redirect(url);
       } else {
         var calendar = google.calendar('v3');
         calendar.events.list({
-          calendarId    : 'primary',
-          maxResults    : 10,
-          timeMin       : (new Date()).toISOString(),
-          singleEvents  : true,
-          orderBy       : 'startTime',
-          auth          : oAuthClient
+          calendarId: 'primary',
+          maxResults: 10,
+          timeMin: (new Date()).toISOString(),
+          singleEvents: true,
+          orderBy: 'startTime',
+          auth: oAuthClient
         }, function(err, response) {
           if (err) {
             console.log('The API returned an error: ' + err);
@@ -39,20 +41,26 @@ module.exports = function(app, config) {
               err: 'No upcoming events found.'
             });
           } else {
-            console.log('Upcoming 10 events: ', events);
+            console.log('Upcoming 10 events: ');
             var selectedEvents = [];
             for (var i = 0; i < events.length; i++) {
               var event = events[i],
-                eventDetails = {};
-              eventDetails.start      = event.start.dateTime || event.start.date;
-              eventDetails.end        = event.end.dateTime || event.end.date;
-              eventDetails.status     = event.status;
-              eventDetails.location   = event.location;
-              eventDetails.organiser  = event.organiser;
-              eventDetails.summary    = event.summary;
+                eventDetails = {
+                  start: event.start.dateTime ? event.start.dateTime : "1",
+                  end: event.end.dateTime ? event.end.dateTime : "1",
+                  status: event.status ? event.status : "1",
+                  location: event.location ? event.location : "1",
+                  organiser: event.organiser ? event.organiser : "1",
+                  summary: event.summary ? event.summary : "1"
+                };
               selectedEvents.push(eventDetails);
             }
-            return res.json(selectedEvents);
+            console.log(selectedEvents, "uid", uid);
+            root.child("calendars").child(uid).set(selectedEvents, function(err) {
+              if (!err) {
+                return res.json(selectedEvents);
+              }
+            });
           }
         });
       }
@@ -63,14 +71,12 @@ module.exports = function(app, config) {
       var code = req.query.code;
       oAuthClient.getToken(code, function(err, tokens) {
         if (err) {
-          console.log('Error authenticating');
-          console.log(err);
+          console.log('Error authenticating', err);
         } else {
-          console.log('Successfully authenticated');
-          console.log(tokens);
+          console.log('Successfully authenticated', tokens);
           oAuthClient.setCredentials(tokens);
           authed = true;
-          res.redirect('/calendar');
+          res.redirect('/' + uid + '/calendar');
         }
       });
     });
