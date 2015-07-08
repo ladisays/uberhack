ca24da6d20c159be9ceab44fa93707e6bf1a39e0
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-08 14:53:43 +0100

    fix bug

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 var users = require('./users.routes'),
   calendar = require('./calendar.routes'),
   requests = require('./requests.routes'),
-  shared = require('./shared.routes');
-location = require('./location.routes');
+  shared = require('./shared.routes'),
+  location = require('./location.routes');
 
 module.exports = function(app, config) {
   users(app, config);
