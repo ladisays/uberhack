0e69d16e4f4c64bc092847c2a1de074e94e1467c
Kingsley Ochu <kingsley.ochu@andela.co>
2015-07-08 15:15:10 +0100

    created location route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,9 @@
-var users 			= require('./users.routes'),
-		calendar 		= require('./calendar.routes'),
-		requests 		= require('./requests.routes'),
-		shared 			= require('./shared.routes'),
-		keepalive		= require('./keep-alive.routes');
+var users = require('./users.routes'),
+  calendar = require('./calendar.routes'),
+  requests = require('./requests.routes'),
+  shared = require('./shared.routes'),
+  keepalive = require('./keep-alive.routes'),
+  location = require('./location.routes');
 
 module.exports = function(app, config) {
   users(app, config);
@@ -10,4 +11,5 @@ module.exports = function(app, config) {
   requests(app, config);
   shared(app, config);
   keepalive(app, config);
+  location(app, config);
 };
